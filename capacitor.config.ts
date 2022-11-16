f54401f355915118ccf06a1728c46c2fc0ed8cd2
Thiago Siqueira <thiago.siqueira@getambush.com>
2022-11-16 13:48:37 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-tabs-preloading-strategy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
